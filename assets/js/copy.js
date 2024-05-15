d168e6318237087202d4517cf93053649cbb26be
Max Maltsev <malt1640@ya.ru>
2024-05-15 23:40:33 +0300

    Добавил возможность скопировать ссылку на пьесу

---
+++ assets/js/copy.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.piece-card__copy').forEach(button => {
+  button.addEventListener('click', function() {
+      // Получаем id родительского блока
+      const pieceCard = this.closest('.piece-card');
+      const pieceId = pieceCard.id;
+      // Создаем якорную ссылку
+      const anchorLink = `${window.location.href.split('#')[0]}#${pieceId}`;
+
+      // Копируем якорную ссылку в буфер обмена
+      navigator.clipboard.writeText(anchorLink).then(() => {
+          // Меняем текст кнопки
+          button.textContent = 'Скопировано';
+
+          // Возвращаем исходный текст через 3 секунды
+          setTimeout(() => {
+              button.textContent = 'Копировать ссылку';
+          }, 5000);
+      }).catch(err => {
+          console.error('Failed to copy: ', err);
+      });
+  });
+});
